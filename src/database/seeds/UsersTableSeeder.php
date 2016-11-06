178a45aa1401286d54efe34854a2b7f5484274f3
lephleg <lephleg@gmail.com>
2016-11-06 21:12:30 +0200

    Fix database migration error, Update admin password, Drop rows on seed.

--- src/database/seeds/UsersTableSeeder.php
+++ src/database/seeds/UsersTableSeeder.php
@@ -7,9 +7,11 @@ class UsersTableSeeder extends Seeder {
 
     public function run()
     {
+        DB::table('users')->delete();
+
         $legitUser = new \App\User;
         $legitUser->username = 'admin';
-        $legitUser->password = Hash::make('.DOTsoft,2016');
+        $legitUser->password = Hash::make('DOTsoft123');
         $legitUser->save();
     }
 
