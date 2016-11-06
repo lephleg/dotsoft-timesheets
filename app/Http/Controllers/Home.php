3e363c79f5a13215b717a6ef41fa954e44300543
lephleg <lephleg@gmail.com>
2016-11-06 19:23:21 +0200

    Remove title from home page.

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -13,7 +13,7 @@ class Home extends Controller {
     public function index() {
 
 
-        return view('home', array('pageTitle' => 'Daily Timesheets'));
+        return view('home', array('pageTitle' => null));
     }
 
 
